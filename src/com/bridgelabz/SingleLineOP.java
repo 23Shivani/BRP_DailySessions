1e3758308734a8d257874c4e223bcdcbd00928ce
23Shivani <shivani.gharve.2310@gmail.com>
2021-10-05 20:50:16 +0530

    Added: Printing pattern as output program

---
+++ src/com/bridgelabz/SingleLineOP.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class SingleLineOP {
+	public static void main(String[] args) 
+	{
+	 int i,j,k;
+	for(i=1; i<=5; i++)
+	{
+	for(j=1; j<=8; j++)
+	{
+	System.out.print("-");
+	}
+	System.out.println(" ");
+	}
+	}
+}
