b3013c7c5757e87de71b1c65e7b77b13086fb90a
23Shivani <shivani.gharve.2310@gmail.com>
2021-10-08 09:50:22 +0530

    Added: results Multilevel Inheritance Program

---
+++ src/com/bridgelabz/MultiLevelInheritance.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+	class GrandFather {
+		  GrandFather() {
+		    System.out.println("I am the grandfather!");
+		  }
+		}
+		class Father extends GrandFather {
+		  String familyName;
+		  String houseaddress;
+		  Father() {
+		    System.out.println("I am the father! I inherit from Grandfather");
+		  }
+		}
+		public class MultiLevelInheritance extends Father {
+		  MultiLevelInheritance() {
+		    System.out.println("I am the son and I inherit from my father.");
+		  }
+		  public static void main(String[] args) {
+		    MultiLevelInheritance s1 = new MultiLevelInheritance();
+		  }
+		}
