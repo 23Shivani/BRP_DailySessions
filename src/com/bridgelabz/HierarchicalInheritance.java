a1fd9e9f6de1a8a9621dc0fcf8efa1582d8d2882
23Shivani <shivani.gharve.2310@gmail.com>
2021-10-08 09:49:41 +0530

    Added: results hierarchical Inheritance Program

---
+++ src/com/bridgelabz/HierarchicalInheritance.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+	class Father {
+		String familyName;
+		String houseaddress;
+		Father() {
+			familyName = "Programmer";
+			houseaddress = "Delhi";
+		}
+	}
+	class Son extends Father {
+		Son() {
+			System.out.println("I am the Son");
+			System.out.println("My family name is " + this.familyName + " and I am from " + this.houseaddress);
+		}
+	}
+	class Daughter extends Father {
+		Daughter() {
+			System.out.println("I am the Daughter");
+		    System.out.println("My family name is " + this.familyName + " and I am from " + this.houseaddress);
+	    }
+	}
+		
+	public class HierarchicalInheritance {
+		public static void main(String[] args) {
+			Son s = new Son();
+			Daughter d = new Daughter();
+		}
+	}
