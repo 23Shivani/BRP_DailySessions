a81ae91d8140d437d7703f041ca9843c6b37a255
23Shivani <shivani.gharve.2310@gmail.com>
2021-10-09 09:50:18 +0530

    Added: getter-setter methods of printing name and address using encapsulation Program

---
+++ src/com/bridgelabz/Encapsulation1.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+public class Encapsulation1 {
+
+	private String firstName;
+	private String LastName;
+	private String Address;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	
+	public String getLastName() {
+		return LastName;
+	}
+	
+	
+	public String getAddress() {
+		return Address;
+	}
+	
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+	public void setLastName(String lastName) {
+		this.LastName = lastName;
+	}
+	
+	
+	public void setAddress(String address) {
+		this.Address = address;
+	}
+	
+	
+}
