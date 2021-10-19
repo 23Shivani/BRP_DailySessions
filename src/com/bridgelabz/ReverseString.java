8905f9754bde70825ca732f18f92fecd2e19d41c
23Shivani <shivani.gharve.2310@gmail.com>
2021-10-19 08:31:33 +0530

    Added: Reversing string Program

---
+++ src/com/bridgelabz/ReverseString.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class ReverseString {  
+public static String reverseString(String str){  
+    char ch[]=str.toCharArray();  
+    String rev="";  
+    for(int i=ch.length-1;i>=0;i--){  
+        rev+=ch[i];  
+    }  
+    return rev;  
+}  
+public class TestStringFormatter {  
+public static void main(String[] args) {  
+    System.out.println(ReverseString.reverseString("my name is khan"));  
+    System.out.println(ReverseString.reverseString("I am sonoo jaiswal"));      
+    }  
+}
+}
