d03c07b8064aeac5683d4084149f2848224e7890
23Shivani <shivani.gharve.2310@gmail.com>
2021-10-19 08:29:33 +0530

    Added: Main Method Polymorphism Program

---
+++ src/com/bridgelabz/AreaOfSquarePolymorphism.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AreaOfSquarePolymorphism {
+	 public static void main(String args[]){
+	      int length, area;
+	      Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter the length of the square ::");
+	      length = sc.nextInt();
+	      area = length* length;
+	      System.out.println("Area of the square is ::"+area);
+	   }
+}
