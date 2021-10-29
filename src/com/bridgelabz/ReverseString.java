da53cfb2f4919e20deeb58ed68a1d7a6fd801255
23Shivani <shivani.gharve.2310@gmail.com>
2021-10-29 22:44:57 +0530

    Added: reverse string Program

--- src/com/bridgelabz/ReverseString.java
+++ src/com/bridgelabz/ReverseString.java
@@ -1,18 +1,35 @@
 package com.bridgelabz;
 
 public class ReverseString {  
-public static String reverseString(String str){  
-    char ch[]=str.toCharArray();  
-    String rev="";  
-    for(int i=ch.length-1;i>=0;i--){  
-        rev+=ch[i];  
-    }  
-    return rev;  
-}  
-public class TestStringFormatter {  
-public static void main(String[] args) {  
-    System.out.println(ReverseString.reverseString("my name is khan"));  
-    System.out.println(ReverseString.reverseString("I am sonoo jaiswal"));      
-    }  
-}
+	public static void main(String[] args) {
+		String test = "Hello My Name Is Shivani";
+		System.out.println("String: " +test);
+		System.out.println("Reverse using reverseStringBuffer:" + reverseStringBuffer(test));
+		System.out.println("Reverse using reverseCharArray:" + reverseCharArray(test));
+		System.out.println("Reverse using reverseRecursion:" + reverseRecursion(test));
+	}
+
+	private static String reverseStringBuffer(String str) {
+		// TODO Auto-generated method stub
+		return new StringBuffer(str).reverse().toString();
+	}
+
+	public static String reverseCharArray(String st) {
+	    char[] reverseStringArray = new char[st.length()];
+	    for (int i = st.length() - 1, j = 0; i != -1; i--, j++) {
+	    	//Returns char value at particular index ranging from 0 to length()-1.
+			//first char value of the sequence is at index 0,next at index 1,as for array indexing.
+	        reverseStringArray[j] = st.charAt(i);
+	    }
+	    return new String(reverseStringArray);
+	}
+
+	public static String reverseRecursion(String st) {
+	    if (st.length() <= 1) {
+	        return st;
+	    }
+	    //Returns a string that is a substring of this string.
+	    //substr begin at the firstIndex and extends to the char at index lastIndex - 1.
+		return reverseRecursion(st.substring(1, st.length())) + st.charAt(0);
+	}
 }
